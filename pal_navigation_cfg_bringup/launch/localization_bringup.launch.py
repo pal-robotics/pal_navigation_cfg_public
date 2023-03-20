daf74438b0d69bac1c7b52809ddd84665878fc52
antoniobrandi <antonio.brandi@pal-robotics.com>
2023-03-20 12:11:59 +0100

    added default parameters and launch files

--- pal_navigation_cfg_bringup/launch/localization_bringup.launch.py
+++ pal_navigation_cfg_bringup/launch/localization_bringup.launch.py
@@ -1,48 +1,60 @@
-# Copyright (c) 2023 PAL Robotics S.L. All rights reserved.
+# Copyright (c) 2022 PAL Robotics S.L. All rights reserved.
 #
-# Unauthorized copying of this file, via any medium is strictly prohibited,
-# unless it was supplied under the terms of a license agreement or
-# nondisclosure agreement with PAL Robotics SL. In this case it may not be
-# copied or disclosed except in accordance with the terms of that agreement.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
+import yaml
+from typing import Dict
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import (
     IncludeLaunchDescription,
     DeclareLaunchArgument,
+    OpaqueFunction,
 )
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import SetRemap
+from launch_pal.param_utils import parse_parametric_yaml
 
 
-def generate_launch_description():
+def loc_and_nav(context, *args, **kwargs):
+    remappings_file = LaunchConfiguration("remappings_file").perform(context)
+    params_file = LaunchConfiguration("params_file").perform(context)
     map_yaml_file = LaunchConfiguration("map")
-    configured_params = LaunchConfiguration("params_file")
 
     nav2_bringup_pkg = os.path.join(
         get_package_share_directory("nav2_bringup"), "launch"
     )
-    pmb2_maps_dir = get_package_share_directory("pmb2_maps")
 
-    declare_params_file_launch_arg = DeclareLaunchArgument(
-        "params_file",
-        default_value=os.path.join(
-            get_package_share_directory("pal_navigation_cfg_params"),
-            "localization",
-            "localization.yaml",
-        ),
-        description=(
-            "Full path to the ROS2 parameters file to use for Localization"
-            " with AMCL configuration"
-        ),
-    )
+    source_files = []
 
-    declare_map_yaml_cmd = DeclareLaunchArgument(
-        "map",
-        default_value=os.path.join(pmb2_maps_dir, "config", "map.yaml"),
-        description="Full path to map yaml file to load",
+    with open(remappings_file, "r") as remappings_yaml:
+        remappings = yaml.safe_load(remappings_yaml)
+
+    with open(params_file, "r") as params_yaml:
+        params: Dict = yaml.safe_load(params_yaml)
+        for key, param in params.items():
+            source_files.append(
+                os.path.join(
+                    get_package_share_directory("pal_navigation_cfg_params"),
+                    key,
+                    param + ".yaml",
+                )
+            )
+
+    configured_params = parse_parametric_yaml(
+        source_files=source_files, param_rewrites=remappings
     )
 
     localization_launch = IncludeLaunchDescription(
@@ -63,6 +75,43 @@ def generate_launch_description():
         ),
         launch_arguments={"params_file": configured_params}.items(),
     )
+    return [localization_launch, cmd_vel_remap, nav2_bringup_launch]
+
+
+def generate_launch_description():
+    nav2_bringup_pkg = os.path.join(
+        get_package_share_directory("nav2_bringup"), "launch"
+    )
+    pmb2_maps_dir = get_package_share_directory("pmb2_maps")
+
+    declare_params_file_launch_arg = DeclareLaunchArgument(
+        "params_file",
+        description=(
+            "Full path to the ROS2 parameters file to use for AMCL"
+            " configuration"
+        ),
+    )
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        "map",
+        default_value=os.path.join(pmb2_maps_dir, "config", "map.yaml"),
+        description="Full path to map yaml file to load",
+    )
+
+    declare_remappings_file_launch_arg = DeclareLaunchArgument(
+        "remappings_file",
+        default_value=os.path.join(
+            get_package_share_directory("pal_navigation_cfg_bringup"),
+            "params",
+            "default_remappings.yaml",
+        ),
+        description=(
+            "Full path to the ROS2 parameters file to use for NAV2"
+            " configuration"
+        ),
+    )
+
+    loc_and_nav_launch = OpaqueFunction(function=loc_and_nav)
 
     rviz_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([nav2_bringup_pkg, "/rviz_launch.py"])
@@ -72,10 +121,9 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     ld.add_action(declare_params_file_launch_arg)
+    ld.add_action(declare_remappings_file_launch_arg)
     ld.add_action(declare_map_yaml_cmd)
-    ld.add_action(localization_launch)
-    ld.add_action(cmd_vel_remap)
-    ld.add_action(nav2_bringup_launch)
+    ld.add_action(loc_and_nav_launch)
     ld.add_action(rviz_launch)
 
     return ld
